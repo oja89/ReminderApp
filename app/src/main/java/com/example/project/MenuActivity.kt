e64c62f1e7c037a33944e472a293d7c19e65d0c9
oja89 <56597978+oja89@users.noreply.github.com>
2021-02-09 18:29:48 +0200

    Simple listview

--- app/src/main/java/com/example/project/MenuActivity.kt
+++ app/src/main/java/com/example/project/MenuActivity.kt
@@ -1,11 +1,21 @@
 package com.example.project
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 
 class MenuActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_menu)
+
+        // string for listview
+        val myStringArray = arrayOf<String>("ased","adsfa","asdfla")
+
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myStringArray)
+        val listView: ListView = findViewById(R.id.mainList)
+        listView.adapter = adapter
+
     }
 }
