823286382518b1b6d693b934f215a85182edb5b9
oja89 <56597978+oja89@users.noreply.github.com>
2021-02-23 21:08:47 +0200

    Tweaks after HW1, uses binding now

--- app/src/main/java/com/example/project/MainActivity.kt
+++ app/src/main/java/com/example/project/MainActivity.kt
@@ -2,17 +2,36 @@ package com.example.project
 
 import android.content.Intent
 import android.os.Bundle
-import android.util.Log
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
+import com.example.project.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    // binding
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        findViewById<Button>(R.id.btnLogin).setOnClickListener {
-            Log.d("lab", "login clicked")
+        // create binding
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        //setContentView(R.layout.activity_main)
+        setContentView(view)
+
+        // login button functions
+        //findViewById<Button>(R.id.btnLogin).setOnClickListener {
+        binding.btnLogin.setOnClickListener {
+            var user = R.id.txtUsername
+            var pass = R.id.txtPassword
+
+            /// could not make this into HW1 deadline
+            //applicationContext.getSharedPreferences(
+            //        getString(R.string.sharedPreference),
+             //       Context.MODE_PRIVATE
+            //).edit().putInt("LoginStatus", 1).apply()
+
+
+            // finally start menu activity
             startActivity(
                     Intent(applicationContext, MenuActivity::class.java)
             )
--- app/src/main/java/com/example/project/MenuActivity.kt
+++ app/src/main/java/com/example/project/MenuActivity.kt
@@ -3,15 +3,25 @@ package com.example.project
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ArrayAdapter
-import android.widget.Button
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
-import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.example.project.databinding.ActivityMenuBinding
+import kotlinx.android.synthetic.main.activity_menu.view.*
 
 class MenuActivity : AppCompatActivity() {
+    // binding
+    private lateinit var binding: ActivityMenuBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_menu)
+
+
+        //binding
+        // create binding
+        binding = ActivityMenuBinding.inflate(layoutInflater)
+        val view = binding.root
+
+        //setContentView(R.layout.activity_menu)
+        setContentView(view)
 
         // string for listview
         val myStringArray = arrayOf<String>("This", "is", "placeholder")
@@ -20,19 +30,21 @@ class MenuActivity : AppCompatActivity() {
         // adapter for strings
         val adapter = ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, myStringArray)
         // call for setAdapter...
-        val myListView: ListView = findViewById(R.id.mainList)
+        //val myListView: ListView = findViewById(R.id.mainList)
+        val myListView: ListView = view.mainList
         myListView.adapter = adapter
 
         // button for profile
-        findViewById<Button>(R.id.btnProfile).setOnClickListener {
+        //findViewById<Button>(R.id.btnProfile).setOnClickListener {
+        view.btnProfile.setOnClickListener{
             startActivity(
                     Intent(applicationContext, ProfileActivity::class.java)
             )
-
         }
 
         // button for logout
-        findViewById<Button>(R.id.btnLogout).setOnClickListener {
+        //findViewById<Button>(R.id.btnLogout).setOnClickListener {
+        view.btnLogout.setOnClickListener{
             // set logged in -> false?
             startActivity(
                     Intent(applicationContext, MainActivity::class.java)
@@ -40,7 +52,8 @@ class MenuActivity : AppCompatActivity() {
         }
 
         // button for adding new items to list
-        findViewById<FloatingActionButton>(R.id.btnAdd).setOnClickListener {
+        //findViewById<FloatingActionButton>(R.id.btnAdd).setOnClickListener {
+        view.btnAdd.setOnClickListener {
             startActivity(
                 Intent(applicationContext, ProfileActivity::class.java)
             )
